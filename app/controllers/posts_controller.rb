c11d8aea21e2823ec9baa6b79bd690ffec7eaa5d
isymkz44 <falot.323.66@yahoo.ne.jp>
2022-12-13 15:38:45 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,10 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    # 新たに投稿されたメモの内容を変数postに格納
+    render json:{ post: post }
+    # jsonオプションにより、直後に記述した{ post: post }というデータをJSON形式で返却する
+    # 定義した変数postの値を、postというキーとセットでJavaScriptに送信
   end
 end
