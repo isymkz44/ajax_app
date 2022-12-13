8087b98d4347cc1886f9629c4d4fa6955c1db1ae
isymkz44 <falot.323.66@yahoo.ne.jp>
2022-12-13 16:09:00 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,20 +1,37 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    // 投稿ボタンがクリックされることをトリガーにして、非同期通信でメモが投稿されるように設定
     const form = document.getElementById("form");
-    // getElementByIdメソッドで取得したフォームの要素を変数formに格納
     const formData = new FormData(form);
-    // フォームの値を取得、新たに生成したFormDataオブジェクトを変数formDataに格納
     const XHR = new XMLHttpRequest();
-    // 非同期通信を行うためにXMLHttpRequestオブジェクトを生成
     XHR.open("POST", "/posts", true);
-    // リクエストの内容を指定。非同期で投稿したメモをデータベースに保存したいので、HTTPメソッドにはPOSTを指定
     XHR.responseType = "json";
-    // 受け取るレスポンスのデータフォーマットを指定。
     XHR.send(formData);
-    // フォームに入力された内容をサーバー側に送信
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
